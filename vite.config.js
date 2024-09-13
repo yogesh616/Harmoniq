afd49ff3d7b5bdddbd5f432433f3575c955b93e1
yogesh616 <71480544+yogesh616@users.noreply.github.com>
2024-09-13 20:30:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/search': 'https://jiosaavn-api-privatecvc2.vercel.app'
-    }
-  }
 })
